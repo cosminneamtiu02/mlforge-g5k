1f7e759a3de484b3b7136b64319385a0facdc1da
Neamtiu Cosmin <91669989+cosminneamtiu02@users.noreply.github.com>
2025-07-17 18:53:11 +0300

    Update bad.sh

--- scripts/bad.sh
+++ scripts/bad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This is a bad shell script
 
-echo $UNQUOTED   # Unquoted variable
-cd /tmp          # cd without checking result
-test $1 == foo   # Non-POSIX 'test' usage
+echo $UNQUOTED   
+cd /tmp          
+test $1 == foo   
